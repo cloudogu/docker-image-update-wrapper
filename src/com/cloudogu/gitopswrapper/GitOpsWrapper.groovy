6c99ca08ae2e78c65ca7cb12aebfd05d70032c19
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 12:12:54 +0100

    Update GitOpsWrapper.groovy

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -1,15 +1,22 @@
 package com.cloudogu.gitopswrapper
 
 class GitOpsWrapper {
-    def script  // Pipeline context
+    def script
 
     GitOpsWrapper(def script) {
         this.script = script
     }
 
     def deploy(Map gitopsConfig) {
-        def git = script.Git()  // Call the Git() pipeline step directly
-        script.echo "Using Git instance in deploy"
-        script.deployViaGitops(gitopsConfig)
+        def cesLib = script.library(identifier: 'github.com/cloudogu/ces-build-lib@main').load()
+        def git = cesLib.Git(script)  // Create an isolated instance of Git
+
+        script.echo "Using isolated Git instance in deploy"
+        git.checkout(
+            gitopsConfig.scm.repositoryUrl,
+            'main',
+            gitopsConfig.scm.credentialsId
+        )
+        script.deployViaGitops(gitopsConfig)  // Call deployViaGitops with isolated context
     }
 }
