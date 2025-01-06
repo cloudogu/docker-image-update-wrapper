550ab81cfd95923f23edfd6f916bf9c545c8c93c
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 12:55:48 +0100

    Update GitOpsWrapper.groovy

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -8,22 +8,30 @@ class GitOpsWrapper {
     }
 
     def deploy(Map gitopsConfig) {
-        // Load the entire library for isolation
-        def cesLib = script.library(identifier: 'github.com/cloudogu/ces-build-lib@main').load()
-
-        // Access `Git` as a function (since it's in `vars`)
-        def git = cesLib.Git  // Reference to the `Git` step, no `new` required
+        def cesLibWrapper = new CesBuildLibWrapper(script)  // Create new instance of nested class
+        def git = cesLibWrapper.git()  // Get Git reference
 
         script.echo "Using isolated Git instance for deployment"
 
-        // Call the `Git` step
         git.checkout(
             gitopsConfig.scm.repositoryUrl,
             'main',
             gitopsConfig.scm.credentialsId
         )
 
-        // Call deployViaGitops within the isolated environment
         script.deployViaGitops(gitopsConfig)
+    }
+
+    // Nested class for `ces-build-lib` isolation
+    class CesBuildLibWrapper {
+        def script
+
+        CesBuildLibWrapper(def script) {
+            this.script = script
+        }
+
+        def git() {
+            return this.script.Git  // Return reference to Git step
+        }
     }
 }
