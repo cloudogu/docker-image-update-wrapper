03da61013142565112c01dcd9a79e92e2f8940d4
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 12:07:36 +0100

    Update GitOpsWrapper.groovy
    test

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -1,16 +1,15 @@
 package com.cloudogu.gitopswrapper
 
 class GitOpsWrapper {
-    def script  // Holds the pipeline context (`this`)
-    def git  // Holds a new Git instance
+    def script  // Pipeline context
 
     GitOpsWrapper(def script) {
         this.script = script
-        this.git = new com.cloudogu.ces.cesbuildlib.Git(script)  // Create a new instance of the Git class for this wrapper
     }
 
     def deploy(Map gitopsConfig) {
-        script.echo "Deploying with a new Git instance"
+        def git = script.Git()  // Call the Git() pipeline step directly
+        script.echo "Using Git instance in deploy"
         script.deployViaGitops(gitopsConfig)
     }
 }
