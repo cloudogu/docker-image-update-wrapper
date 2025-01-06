e0ac619287a054175e03c57a9c40916bf56d33cb
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 14:23:48 +0100

    Update GitOpsWrapper.groovy

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -9,6 +9,7 @@ class GitOpsWrapper implements Serializable {
 
     def deploy(Map gitopsConfig) {
         script.library('github.com/cloudogu/gitops-build-lib@0.7.0')  // Reload ces-build-lib
-        script.deployViaGitops(gitopsConfig)  // Call global function with context
+        def deployViaGitops = this.script.deployViaGitops  // Pull reference into closure
+        deployViaGitops(gitopsConfig)  // Call global function with context
     }
 }
