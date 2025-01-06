838e6849a8f5a016751f911d5f3ae8459b4c5a8c
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 13:39:03 +0100

    Update GitOpsWrapper.groovy

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -1,37 +1,10 @@
 package com.cloudogu.gitopswrapper
 
+import com.cloudogu.gitops.GitopsBuildLib
+
 class GitOpsWrapper {
-    def script
-
-    GitOpsWrapper(def script) {
-        this.script = script
-    }
-
     def deploy(Map gitopsConfig) {
-        def cesLibWrapper = new CesBuildLibWrapper(script)  // Create new instance of nested class
-        def git = cesLibWrapper.git()  // Get Git reference
-
-        script.echo "Using isolated Git instance for deployment"
-
-        git.checkout(
-            gitopsConfig.scm.repositoryUrl,
-            'main',
-            gitopsConfig.scm.credentialsId
-        )
-
-        script.deployViaGitops(gitopsConfig)
-    }
-
-    // Nested class for `ces-build-lib` isolation
-    class CesBuildLibWrapper {
-        def script
-
-        CesBuildLibWrapper(def script) {
-            this.script = script
-        }
-
-        def git() {
-            return this.script.Git  // Return reference to Git step
-        }
+        def gitOpsLib = new GitopsBuildLib()  // Create a new instance of GitOpsBuildLib
+        gitOpsLib.deployViaGitops(gitopsConfig)  // Call the deployViaGitops function
     }
 }
