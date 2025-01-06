8d2d610faf4c8784727c7d24592743b384f2caf2
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 12:35:05 +0100

    Update GitOpsWrapper.groovy

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -12,17 +12,8 @@ class GitOpsWrapper {
         def cesLib = script.library(identifier: 'github.com/cloudogu/ces-build-lib@main').load()
 
         // Access the Git class and other classes from the library
-        def git = new cesLib.com.cloudogu.ces.cesbuildlib.Git(script)  // Isolated instance of `Git`
         script.echo "Using isolated Git instance in deploy"
 
-        git.checkout(
-            gitopsConfig.scm.repositoryUrl,
-            'main',
-            gitopsConfig.scm.credentialsId
-        )
-
-        // Example: If you want to call another utility class from the lib
-        // def maven = new cesLib.com.cloudogu.ces.cesbuildlib.Maven(script)
 
         script.deployViaGitops(gitopsConfig)
     }
