b37a0a47429c20009282f5e25d61b1a7e9cdc4e9
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-01-06 13:46:14 +0100

    Update GitOpsWrapper.groovy
    test

--- src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
+++ src/com/cloudogu/gitopswrapper/GitOpsWrapper.groovy
@@ -1,10 +1,11 @@
 package com.cloudogu.gitopswrapper
 
-import com.cloudogu.gitops.GitopsBuildLib
+
+import com.cloudogu.gitops.gitopsbuildlib
 
 class GitOpsWrapper {
     def deploy(Map gitopsConfig) {
-        def gitOpsLib = new GitopsBuildLib()  // Create a new instance of GitOpsBuildLib
+        def gitOpsLib = new gitopsbuildlib()  // Create a new instance of GitOpsBuildLib
         gitOpsLib.deployViaGitops(gitopsConfig)  // Call the deployViaGitops function
     }
 }
